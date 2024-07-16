f8a72d312040bb49ae1a263556f8376df26f08fd
shah 911 <mianshahzeb911@gmail.com>
2024-07-16 14:23:50 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@ import FramerMagneticEffect from "@/components/FramerMagneticEffect";
 // import MagneticEffect from "@/components/Magnet";
 import Navbar from "@/components/Navbar";
 
+const words = ["Magnetic", "Hover", "Effect"];
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <div className="fixed top-0">
         <Navbar />
       </div>
-      <FramerMagneticEffect>
-        <span className="text-2xl md:text-4xl cursor-pointer">Magnetic</span>
-      </FramerMagneticEffect>
-      <FramerMagneticEffect>
-        <span className="text-2xl md:text-4xl cursor-pointer">Hover</span>
-      </FramerMagneticEffect>
-      <FramerMagneticEffect>
-        <span className="text-2xl md:text-4xl cursor-pointer">Effect</span>
-      </FramerMagneticEffect>
-      <p className="text-center mt-5">
+      {words.map((item, i) => (
+        <FramerMagneticEffect key={i}>
+          <span className="text-2xl md:text-4xl cursor-pointer 2xl:leading-[125%] 2xl:text-[3vw]">
+            {item}
+          </span>
+        </FramerMagneticEffect>
+      ))}
+      <p className="text-center mt-5 2xl:mt-[3vw] 2xl:text-[1.25vw]">
         Hover over the nav links or words to observe the animation.
       </p>
     </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,18 @@ import React from "react";
 // import MagneticEffect from "./Magnet";
 import FramerMagneticEffect from "./FramerMagneticEffect";
 
-const links = ["about", "contact", "work"];
+const links = ["work", "about", "contact"];
 
 function Navbar() {
   return (
-    <div className="w-[95vw] py-4 mx-auto flex items-center justify-center">
+    <div className="w-[95vw] py-4 2xl:py-[2vw] mx-auto flex flex-col md:flex-row items-center justify-center gap-4 md:gap-0">
       <div className="flex-[1]">
-        <span className="text-2xl">shah.</span>
+        <span className="text-2xl 2xl:text-[1.75vw] capitalize">shah.</span>
       </div>
-      <div className="flex-[1] flex items-center justify-end gap-10">
+      <div className="flex-[1] flex items-center justify-end gap-5 md:gap-10 2xl:gap-[4vw]">
         {links.map((item, i) => (
           <FramerMagneticEffect key={i}>
-            <span className="cursor-pointer text-sm border border-black py-2 px-4 rounded-full transition-colors duration-300 hover:text-white hover:bg-black">
+            <span className="cursor-pointer capitalize text-sm border border-black py-2 px-4 2xl:px-[1.5vw] 2xl:py-[0.75vw] 2xl:text-[1vw] rounded-full transition-colors duration-300 hover:text-white hover:bg-black">
               {item}
             </span>
           </FramerMagneticEffect>
